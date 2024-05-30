b6783a3b9035ce5642664a7cb7823badd185926f
basterb12 <basti.herbst1@gmail.com>
2024-05-30 18:04:27 +0100

    Add files via upload

--- Data processing/sensor_file_parser.py
+++ Data processing/sensor_file_parser.py
@@ -8,6 +8,7 @@ class SensorFileParser:
         self.date_format = '%Y-%m-%d%H:%M:%S'
         self.cols_meteo = ['time', 'temperature', 'humidity', 'pressure', 'temperature2']
         self.cols_env = ['time', 'pm_1', 'pm_25', 'pm_10', 'pm_4']
+      #  self.cols_voc = ['time', 'voc']
 
     def parse_file(self, filepath, date) -> (pd.DataFrame, pd.DataFrame):
         f = open(filepath)
@@ -18,6 +19,7 @@ class SensorFileParser:
         line_iter = iter(lines)  # here
         list_meteo = []
         list_env = []
+       # list_voc = []
         for ln in line_iter:
             ln = ln.strip()
             line_data = ln.split(",")
@@ -30,27 +32,37 @@ class SensorFileParser:
 
             if len(line_data) == 4 and line_data[3].startswith("Updated Value of Characteristic"):
                 char_line = line_data[3].split(" ")
-                if len(char_line) > 8:
-                    #characteristic = char_line[4] needed?
+                if len(char_line) > 6:
+                  #  if len(char_line) == 8: #voc values
+                   #     voc = self.byteInt1(char_line[6][0:4])/1000
+                    #    list_voc.append([voc])
                     if len(char_line) == 10:  #short data line (humidity,temp,pressure,temp precision)
-                        temp = self.byteToInt(char_line[6][0:2])
-                        hum = self.byteToInt(char_line[6][2:4])
-                        pressure = self.byteToInt(char_line[7] + char_line[8])/100
-                        temp_prec = self.byteToInt(char_line[9][0:4])/100
+                        temp = self.byteInt1(char_line[6][0:2])
+                        hum = self.byteInt1(char_line[6][2:4])
+                        pressure = self.byteInt1(char_line[7] + char_line[8])/100
+                        temp_prec = self.byteInt1(char_line[9][0:4])/100
                         list_meteo.append([timestamp, hum, temp, pressure, temp_prec])
-                    if len(char_line) == 12: #long data line
-                        pm1 = self.byteToInt(char_line[6] + char_line[7][0:2])/100
-                        pm25 = self.byteToInt(char_line[7][2:4] + char_line[8])/100
-                        pm10 = self.byteToInt(char_line[9] + char_line[10][0:2])/100
-                        pm4 = self.byteToInt(char_line[10][2:4] + char_line[11][0:4])/100
+                    if len(char_line) == 12: #long data line (pm values)
+                        pm1 = self.byteInt1(char_line[6] + char_line[7][0:2])/100
+                        pm25 = self.byteInt1(char_line[7][2:4] + char_line[8])/100
+                        pm10 = self.byteInt1(char_line[9] + char_line[10][0:2])/100
+                        pm4 = self.byteInt1(char_line[10][2:4] + char_line[11][0:4])/100
                         list_env.append([timestamp, pm1, pm25, pm10, pm4])
 
         df_meteo = pd.DataFrame(list_meteo, columns=self.cols_meteo)
         df_env = pd.DataFrame(list_env, columns=self.cols_env)
+     #   df_voc = pd.DataFrame(list_voc, columns=self.cols_voc)
+        
 
-        return df_meteo, df_env
+        return df_meteo, df_env#, df_voc
 
-
+    
+  #this is method used - considers little-endian encoding
+    @staticmethod
+    def byteInt1(x):
+        return int.from_bytes(bytes.fromhex(x), byteorder="little")
+    
+#alternative methods below - tested but didn't seem to match
     @staticmethod
     def byteToInt(x: str) -> int:
         return int.from_bytes(binascii.unhexlify(x), byteorder='little')
